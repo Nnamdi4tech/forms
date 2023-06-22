ed00cd17fa3fa18aea54e992166de4d2af9225b8
Nnamdi4tech <122917052+Nnamdi4tech@users.noreply.github.com>
2023-06-22 09:00:12 -0700

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+
+$email= $_POST['email'];
+
+$message= $_POST['message'];
+
+$number= $_POST['mobile'];
+
+$packs= $_POST['packs'];
+
+$to = "trimmexherbs@gmail.com";
+
+$subject = "Mail From website";
+
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ."
+\r\n Mobile number =". $number . "\r\n No of packs =". $packs;
+
+$headers = "From: noreply@greencoffeehealthplus.space" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
+
